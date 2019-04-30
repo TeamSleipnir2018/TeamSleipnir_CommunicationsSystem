4157546d5b98037f347acc05294597e3021d3e58
orlygur <orlygurolafs@gmail.com>
2019-04-30 10:50:25 +0000

    sends between works !

--- DataLogger/Car.ino
+++ DataLogger/Car.ino
@@ -1,22 +1,18 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
-// Change to 434.0 or other frequency, must match RX's freq!
-#define rf95_FREQ 915.0
-#define RFM69_CS 15
-#define RFM69_RST 25
-#define RFM69_INT 2
+#define RF95_FREQ 434.0
+#define RFM95_CS 15
+#define RFM95_RST 25
+#define RFM95_INT 2
+
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 // Message definitions
 #define ERRORLED 3
-#define REPLY_ACK 0x01 
+#define REPLY_ACK 0x01
 #define REPLY_ERROR 0x03
 #define REPLAY_INITAL 0xFE
-
-// Pit Control caller ID
-const uint8_t Pit_ID = 7;
-// Car Computer caller ID
-const uint8_t Car_ID = 6;
 
 //the command I will be sending: nr.1-command type, nr.2-value and nr.3-error check.
 const int COMMAND_size = 3;
@@ -35,87 +31,19 @@ bool connectionEstablishedToPit = false;
 uint8_t CurrentState = 0;
 uint8_t test = 0;
 
-enum STATE_NAMES
-{
-    TEST,
-    START,
-    READY,
-    RESET
-};
+// Pit Control caller ID
+const uint8_t Pit_ID = 7;
 
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM69_CS, RFM69_INT);
+// Car Computer caller ID
+const uint8_t Car_ID = 6;
 
 void setup()
 {
-    delay(10000);
-    Serial.begin(115200);
-    Serial.println("Race Car Sending Unit!");
-    Serial.println("Setup start");
-    Serial.println("Initializing...");
-    Serial.println("");
-
-    while (!rf95.init())
-    {
-        Serial.println("Initialise failed!");
-        while (1)
-            ;
-    }
-    Serial.println("Initialise achieved!");
-    Serial.println("");
-
-    // Defaults after init are 915.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-    if (!rf95.setFrequency(rf95_FREQ))
-    {
-        Serial.println("setFrequency failed");
-        while (1)
-            ;
-    }
-    Serial.print("Frequency set to: ");
-    Serial.println(rf95_FREQ);
-    Serial.println("");
-
-    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on The default transmitter power is 13dBm, using PA_BOOST. If you are using RFM69/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 5 to 23 dBm:
-    rf95.setModeTx();
-    delay(100);
-    rf95.setTxPower(23, true);
-    Serial.println("Setup Complete.");
-    Serial.println("____________________________________________________________");
-    Serial.println("Establishing connection to Pit....");
-    Serial.println("");
-
-    /*if (establishConnection())
-    {
-        Serial.println("Connection to the Pit established!");
-        Serial.println("");
-        Serial.println("");
-    }
-    else
-    {
-        Serial.println("WARNING: Connection to Pit failed..");
-        Serial.println("");
-        Serial.println("");
-    }*/
-    
-    loop();
-}
-
-void sendMessage(uint8_t CMD_TYPE, uint8_t CMD_VALUE)
-{
-    // Check is the XOR of TYPE and VALUE
-    CMD_CHECK = CMD_VALUE ^ CMD_TYPE;
-    COMMAND[0] = CMD_TYPE;
-    COMMAND[1] = CMD_VALUE;
-    COMMAND[2] = CMD_CHECK;
-
-    if (!rf95.send((uint8_t *)COMMAND, COMMAND_size))
-    {
-        Serial.println("ERROR: Message not able to send!");
-    }
-    Serial.println("Reply Sent!");
-
-    delay(10);
-    //rf95.waitPacketSent();
+    Serial.begin(9600);
+    while (!Serial)
+        ; // Wait for serial port to be available
+    if (!rf95.init())
+        Serial.println("init failed");
 }
 
 void recieveMessage()
@@ -125,11 +53,11 @@ void recieveMessage()
     Serial.println("Waiting for reply...");
 
     // Max wait for data is 1 sec..
-    if (rf95.init())//rf95.waitAvailableTimeout(100))
+    if (rf95.init()) //rf95.waitAvailableTimeout(100))
     {
         if (rf95.recv(buf, &len))
         {
-          Serial.println("Recv is true!");
+            Serial.println("Recv is true!");
             if (rf95.headerId() == Pit_ID)
             {
                 if (buf[2] == (buf[0] ^ buf[1]))
@@ -181,7 +109,7 @@ void recieveMessage()
         Serial.println("WARNING: ACK not received");
     }
     delay(1000);
-    loop();
+    //loop();
 }
 
 bool establishConnection()
@@ -203,12 +131,39 @@ bool establishConnectionReply()
     }
     return false;
 }
-
+void sendMessage(uint8_t CMD_TYPE, uint8_t CMD_VALUE)
+{
+    rf95.setHeaderId(6);
+    uint8_t data = "testing thingy";
+    rf95.send(data, sizeof(data));
+    rf95.waitPacketSent();
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf95.waitAvailableTimeout(3000))
+    {
+        if (rf95.recv(buf, &len))
+        {
+            Serial.print("got reply: ");
+            Serial.println((char *)buf);
+            Serial.print("RSSI: ");
+            Serial.println(rf95.lastRssi(), DEC);
+        }
+        else
+        {
+            Serial.println("recv failed");
+        }
+    }
+    else
+    {
+        Serial.println("No reply, is rf95_server running?");
+    }
+    delay(400);
+}
 void loop()
 {
     Serial.println("____________________________________________________________");
     delay(1000);
     //recieveMessage();
     sendMessage(72, 27);
-    loop();
+    //loop();
 }
--- DataLogger/Pit.ino
+++ DataLogger/Pit.ino
@@ -1,24 +1,13 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
-// Change to 434.0 or other frequency, must match RX's freq!
-#define rf95_FREQ 915.0
-#define RFM69_CS 15
-#define RFM69_RST 25
-#define RFM69_INT 2
-#define ERRORLED 3
-#define REPLY_ACK 0x01
-//#define REPLY_ERROR 0x03
+#define RF95_FREQ 434.0
+#define RFM95_CS 15
+#define RFM95_RST 25
+#define RFM95_INT 2
 
-// Pit Control caller ID
-const uint8_t Pit_ID = 7;
-
-// Car Computer caller ID
-const uint8_t Car_ID = 6;
-
-//two types of commands: set state & set throttle
-//uint8_t CMD_SET_STATE = 0x01;		 // B 0000 0001;
-//uint8_t CMD_SET_THROTTLE = 0x02; // B 0000 0010;
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 //the command I will be sending: nr.1-command type, nr.2-value and nr.3-error check.
 const int COMMAND_size = 3;
@@ -35,144 +24,109 @@ uint8_t DATA_ERROR;
 //Values
 uint8_t CurrentState = 0;
 
-enum STATE_NAMES
-{
-  TEST,
-  START,
-  READY,
-  RESET
-};
+// Pit Control caller ID
+const uint8_t Pit_ID = 7;
 
-// Singleton instance of the radio driver
-RH_RF95 rf95(RFM69_CS, RFM69_INT);
+// Car Computer caller ID
+const uint8_t Car_ID = 6;
 
 void setup()
 {
-  delay(10000);
-	Serial.begin(115200);
-  Serial.println("Setup start");
-  Serial.println("Pit Control Sending Unit!");
-	Serial.println("Initializing");
-  while(!rf95.init())
-	{
-		Serial.println("Initialise failed!");
-		while (1);
-	}
-	Serial.println("Initialise achieved!");
-
-	// Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-	if (!rf95.setFrequency(rf95_FREQ))
-	{
-		Serial.println("setFrequency failed");
-		while (1);
-	}
-
-	Serial.print("Frequency set to: "); Serial.println(rf95_FREQ);
-
-	// Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on The default transmitter power is 13dBm, using PA_BOOST. If you are using RFM69/96/97/98 modules which uses the PA_BOOST transmitter pin, then you can set transmitter powers from 5 to 23 dBm:
-	rf95.setModeRx();
-  delay(100);
-	rf95.setTxPower(23, true);
-  Serial.println("Setup Complete."); Serial.println("____________________________________________________________");
-  
-  recieveMessage();
+    Serial.begin(9600);
+    while (!Serial)
+        ; // Wait for serial port to be available
+    if (!rf95.init())
+        Serial.println("init failed");
 }
 
 void sendMessage(uint8_t CMD_TYPE, uint8_t CMD_VALUE)
 {
-  // check is the XOR of TYPE and VALUE
-  CMD_CHECK = CMD_VALUE ^ CMD_TYPE;
-  COMMAND[0] = CMD_TYPE;
-  COMMAND[1] = CMD_VALUE;
-  COMMAND[2] = CMD_CHECK;
+    // check is the XOR of TYPE and VALUE
+    CMD_CHECK = CMD_VALUE ^ CMD_TYPE;
+    COMMAND[0] = CMD_TYPE;
+    COMMAND[1] = CMD_VALUE;
+    COMMAND[2] = CMD_CHECK;
 
-  if(!rf95.send((uint8_t *)COMMAND, COMMAND_size))
-    Serial.println("ERROR: Message not able to send!");
-  
-  Serial.println("Reply Sent!");
-  delay(10);
-  rf95.waitPacketSent();
-  
-  loop();
+    if (!rf95.send((uint8_t *)COMMAND, COMMAND_size))
+        Serial.println("ERROR: Message not able to send!");
+
+    Serial.println("Reply Sent!");
+    delay(10);
+    rf95.waitPacketSent();
+
+    //loop();
 }
 
 void recieveMessage()
 {
-	uint8_t buf[COMMAND_size] = {0,0,0};
-	uint8_t len = sizeof(buf);
+    uint8_t buf[COMMAND_size] = {0, 0, 0};
+    uint8_t len = sizeof(buf);
 
-  Serial.println("Waiting for reply...");
+    Serial.println("Waiting for reply...");
 
-	// MAX wait for DATA is set 1 sec.
-	if (rf95.init())//.waitAvailableTimeout(10))
-	{
-		if (rf95.recv(buf, &len))
-		{
-      Serial.println("Recv is true!");
-			if (rf95.headerId() == Car_ID)
-			{
-				if (buf[2] == (buf[0] ^ buf[1]))
-				{
-					DATA_TYPE = buf[0];
-					DATA_VALUE = buf[1];
-					DATA_ERROR = buf[2];
+    // MAX wait for DATA is set 1 sec.
+    if (rf95.init()) //.waitAvailableTimeout(10))
+    {
+        if (rf95.recv(buf, &len))
+        {
+            Serial.println("Recv is true!");
+            if (rf95.headerId() == Car_ID)
+            {
+                DATA_TYPE = buf[0];
+                DATA_VALUE = buf[1];
+                DATA_ERROR = buf[2];
 
-					switch (DATA_TYPE)
-					{
-	  				case 0x01:
-		  				Serial.print("#ACK COMMAND: ");
-			  			Serial.println(buf[1]);
-				  		//digitalWrite(ERRORLED, LOW);
-					  	break;
+                /*switch (DATA_TYPE)
+          
+          case 0x01:
+          Serial.print("#ACK COMMAND: ");
+          Serial.println(buf[1]);
+          //digitalWrite(ERRORLED, LOW);
+          
 
-					  default:
-					  Serial.println("#FAILED");
-						  //digitalWrite(ERRORLED, HIGH);
-						  break;
-					}
-        //sendMessage(0, REPLY_ACK);
-				}
-				else
-				{
-					Serial.println("CheackSum ERROR");
-					//digitalWrite(ERRORLED, HIGH);
-				}
-				Serial.println((char *)buf);
-				Serial.print("RSSI: ");
-				Serial.println(rf95.lastRssi(), DEC);
-			}
-			else
-			{
-				Serial.println("Not my message, ID:");
-				Serial.println(rf95.headerId());
-			}
-		}
-		else
-		{
-			Serial.println("Receive failed");
-      Serial.println(buf[0]);
-      Serial.println(buf[1]);
-      Serial.println(buf[2]);
-		}
-	}
-	else
-	{
-		Serial.println("ACK not received");
-	}
- delay(1000);
- loop();
+          default:
+          Serial.println("#FAILED");
+          //digitalWrite(ERRORLED, HIGH);
+          break;*/
+
+                //sendMessage(0, REPLY_ACK);
+
+                Serial.println((char *)buf);
+                Serial.print("RSSI: ");
+                Serial.println(rf95.lastRssi(), DEC);
+            }
+            else
+            {
+                Serial.println("Not my message, ID:");
+                Serial.println(rf95.headerId());
+            }
+        }
+        else
+        {
+            Serial.println("Receive failed");
+            Serial.println(buf[0]);
+            Serial.println(buf[1]);
+            Serial.println(buf[2]);
+        }
+    }
+    else
+    {
+        Serial.println("ACK not received");
+    }
+    delay(1000);
+    //loop();
 }
 
 void loop()
 {
-  Serial.println("____________________________________________________________");
-  
-  delay(1000);
-  if(!rf95.available())
-  {
-    Serial.println("No message available from Car...");
-  }
-  recieveMessage();
-  //sendMessage(Pit_ID, REPLY_ACK);
-  //sendMessage(Car_ID, REPLY_ACK);
+    Serial.println("____________________________________________________________");
+
+    delay(1000);
+    if (!rf95.available())
+    {
+        Serial.println("No message available from Car...");
+    }
+    recieveMessage();
+    //sendMessage(Pit_ID, REPLY_ACK);
+    //sendMessage(Car_ID, REPLY_ACK);
 }
